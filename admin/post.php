94a4d4875b46bd3142844d68b5c8c8dc143a7f47
Robiul Islam <robi.cnits@gmail.com>
2023-07-24 21:07:59 +0600

    1st commit

--- admin/post.php
+++ admin/post.php
@@ -22,31 +22,31 @@
                 $offset = ($page -1) * $limit;
                 //pagination creat end
 
-                // admin all data categery check start
+                // select query of post table for admin user
 
                 if($_SESSION ["user_role"]== '1'){
                 $sql = "SELECT  post.post_id, post.title, post.description,post.post_date,
-                category.category_name,user.username FROM post 
+                category.category_name,user.username,post.category FROM post 
                 LEFT JOIN category ON post.category = category.category_id
                 LEFT JOIN user ON post.author = user.user_id
                 ORDER BY post.post_id DESC LIMIT {$offset},{$limit}";
                 }
 
-                // admin all data categery check end
+                // select query of post table for admin user end
 
 
-                // localuser data categery check start
+                // select query of post table for normal user start
                 
                 elseif($_SESSION ["user_role"]== '0'){
                     $sql = "SELECT  post.post_id, post.title, post.description,post.post_date,
-                category.category_name,user.username FROM post 
+                category.category_name,user.username,post.category FROM post 
                 LEFT JOIN category ON post.category = category.category_id
                 LEFT JOIN user ON post.author = user.user_id
                 WHERE post.author = {$_SESSION['user_id']}
                 ORDER BY post.post_id DESC LIMIT {$offset},{$limit}";
                 }
 
-                // localuser data categery check end
+                // select query of post table for normal user end
 
                 //database and table data add user connection  start
 
@@ -56,7 +56,7 @@
                 
                 ?>
 
-                  <table class="content-table">
+                        <table class="content-table">
                       <thead>
                           <th>S.No.</th>
                           <th>Title</th>
@@ -66,17 +66,22 @@
                           <th>Edit</th>
                           <th>Delete</th>
                       </thead>
-                      <tbody> <?php while($row = mysqli_fetch_assoc($result)){?>
+                      <tbody>
+                        <?php
+                        $serial = $offset + 1;
+                        while($row = mysqli_fetch_assoc($result)) {?>
                           <tr>
-                              <td class='id'><?php echo $row['post_id'];?></td>
-                              <td><?php echo $row['title'];?></td>
-                              <td><?php echo $row['category_name'];?></td>
-                              <td><?php echo $row['post_date'];?></td>
-                              <td><?php echo $row['username'];?></td>
-                              <td class='edit'><a href='update-post.php?id =<?php echo $row['post_id'];?>'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php?id =<?php echo $row['post_id'];?>'><i class='fa fa-trash-o'></i></a></td>
+                              <td class='id'><?php echo $serial; ?></td>
+                              <td><?php echo $row['title']; ?></td>
+                              <td><?php echo $row['category_name']; ?></td>
+                              <td><?php echo $row['post_date']; ?></td>
+                              <td><?php echo $row['username']; ?></td>
+                              <td class='edit'><a href='update-post.php?id=<?php echo $row['post_id']; ?>'><i class='fa fa-edit'></i></a></td>
+                              <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id']; ?>&catid=<?php echo $row['category']; ?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
-                          <?php }?>
+                          <?php
+                          $serial++;
+                        } ?>
                       </tbody>
                   </table>
                   <?php }
@@ -115,11 +120,7 @@
                   }
                   ?>
                   <!-- pagination creat dynamic end -->
-                  <!-- <ul class='pagination admin-pagination'>
-                      <li class="active"><a>1</a></li>
-                      <li><a>2</a></li>
-                      <li><a>3</a></li>
-                  </ul> -->
+                 
               </div>
           </div>
       </div>
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+
+$hostname = " http://localhost/php-practise/news-template";
+
+$connection = mysqli_connect("localhost","root","","news_site") or die("connection failed :" . mysqli_connect_error());
+
+?>
